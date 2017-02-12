0c7e0175046247e80eb9374fee91356dcff30a5f
Autumn Smith <autumn07@gmail.com>
2017-02-12 13:25:37 -0800

    code cleanup

--- server.js
+++ server.js
@@ -2,22 +2,24 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
+
+// client app dir for cleaner code
 const APP_DIR = path.resolve(__dirname, 'src/client');
 
 const app = express();
+
 app.use(morgan('combined'));
 app.use(bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+app.use(bodyParser.urlencoded({    // to support URL-encoded bodies
   extended: true
 }));
 
-app.use(express.static(__dirname + '/src/client'));
+//host static files
+app.use(express.static(APP_DIR));
 
 app.get('/', (req, res) => {
-  console.log(APP_DIR +'/index.html');
   res.sendFile(path.join(APP_DIR + '/index.html'));
 });
-
 
 app.listen(8081, () => {
    console.log('Example app listening at 8081');
