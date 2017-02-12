66d8f20e2ff69a5c7745e1fb4325bd65de177b83
Autumn Smith <autumn07@gmail.com>
2017-02-12 13:18:46 -0800

    serving index.html at /

--- server.js
+++ server.js
@@ -13,16 +13,9 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 app.use(express.static(__dirname + '/src/client'));
 
-app.use('/src',function(req, res, next) {
-  res.send('shut up ');
-});
-//allows for the other files to be loaded
-app.use("/src", express.static(__dirname + '/src'));
-// app.use('/static', express.static(path.join(__dirname,'src')));
-
 app.get('/', (req, res) => {
   console.log(APP_DIR +'/index.html');
-  res.send('hello world');
+  res.sendFile(path.join(APP_DIR + '/index.html'));
 });
 
 
